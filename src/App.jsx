5c2175cf99e86dc75403b69e771fd5832f50074a
Rafaela Umbelino <rafaelaaumbelino@gmail.com>
2023-03-09 15:14:37 +0000

    Autocomplete Works

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  
+
+  const autoCompleteRef = useRef();
+  const inputRef = useRef();
+  const options = {
+   componentRestrictions: { country: "PT" },
+   fields: ["address_components", "geometry", "icon", "name"],
+   types: ["establishment"]
+  };
+  useEffect(() => {
+   autoCompleteRef.current = new window.google.maps.places.Autocomplete(
+    inputRef.current,
+    options
+   );
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <label>enter address :</label>
+   <input ref={inputRef} />
     </div>
   )
 }
