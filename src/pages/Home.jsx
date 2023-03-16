2d37d491e8f40d137bc0e9833fab03daba7d6c4a
Rafaela Umbelino <rafaelaaumbelino@gmail.com>
2023-03-16 21:11:40 +0000

    Final styles + fonts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,23 +3,14 @@ import React from "react";
 function Home() {
   return (
     <div>
-   
     <div className="position-absolute top-50 start-50 translate-middle">
-
-
-    <h1>ROAMING GENIE</h1>
+      <h1 className="text-center">ROAMING GENIE</h1>
+      <br />
+      <h2>A Collaborative Work Experience</h2>
     </div>
+  </div>
+);
 
-    <div className="position-absolute top-50 start-50 translate-middle">
-
-    <h2>A Collaborative Work Experience</h2>
-
-
-    </div>
-    </div>
-     
-   
-  );
 }
 
 export default Home;
