5c241b02e55f808afe7aaece98eb9045dee19fbf
Rafaela Umbelino <rafaelaaumbelino@gmail.com>
2023-03-16 17:39:11 +0000

    background color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,59 +20,68 @@ function Navbar() {
   const [showNavColor, setShowNavColor] = useState(false);
 
   return (
-    <MDBNavbar expand="lg" dark bgColor="dark">
-    <MDBContainer fluid>
-      <MDBNavbarBrand href="/"><img src={cornerGif} alt="Image displaying Work From Anywhere" className="cornerGif" /></MDBNavbarBrand>
-      <MDBNavbarToggler
-        type="button"
-        data-target="#navbarColor02"
-        aria-controls="navbarColor02"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-        onClick={() => setShowNavColor(!showNavColor)}
-      >
-        <MDBIcon icon="bars" fas />
-      </MDBNavbarToggler>
-      <MDBCollapse show={showNavColor} navbar>
-        <MDBNavbarNav className="me-auto mb-2 mb-lg-0">
-          <MDBNavbarItem>
-            <MDBNavbarLink aria-current="page" href="/">
-              Home
-            </MDBNavbarLink>
-          </MDBNavbarItem>
-          {loggedIn && (
-            <>
-              <MDBNavbarItem>
-                <MDBNavbarLink href="/workplaces">Workplaces</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href="/workplaces/new">Add Workplace</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href={`/user/${user._id}`}>User</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <button className="logout-button" onClick={logout}>
-                  Logout
-                </button>
-              </MDBNavbarItem>
-            </>
-          )}
-          {!loggedIn && (
-            <>
-              <MDBNavbarItem>
-                <MDBNavbarLink href="/signup">Signup</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href="/login">Login</MDBNavbarLink>
-              </MDBNavbarItem>
-            </>
-          )}
-        </MDBNavbarNav>
-      </MDBCollapse>
-    </MDBContainer>
-  </MDBNavbar>
-);
+    // <MDBNavbar expand="lg" dark bgColor="dark">
+    <MDBNavbar expand="lg" dark className="bg-dark">
+      <MDBContainer fluid>
+        <MDBNavbarBrand href="/">
+          <img
+            src={cornerGif}
+            alt="Image displaying Work From Anywhere"
+            className="cornerGif"
+          />
+        </MDBNavbarBrand>
+        <MDBNavbarToggler
+          type="button"
+          data-target="#navbarColor02"
+          aria-controls="navbarColor02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={() => setShowNavColor(!showNavColor)}
+        >
+          <MDBIcon icon="bars" fas />
+        </MDBNavbarToggler>
+        <MDBCollapse show={showNavColor} navbar>
+          <MDBNavbarNav className="me-auto mb-2 mb-lg-0">
+            <MDBNavbarItem>
+              <MDBNavbarLink aria-current="page" href="/">
+                Home
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            {loggedIn && (
+              <>
+                <MDBNavbarItem>
+                  <MDBNavbarLink href="/workplaces">Workplaces</MDBNavbarLink>
+                </MDBNavbarItem>
+                <MDBNavbarItem>
+                  <MDBNavbarLink href="/workplaces/new">
+                    Add Workplace
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
+                <MDBNavbarItem>
+                  <MDBNavbarLink href={`/user/${user._id}`}>User</MDBNavbarLink>
+                </MDBNavbarItem>
+                <MDBNavbarItem>
+                  <button className="logout-button" onClick={logout}>
+                    Logout
+                  </button>
+                </MDBNavbarItem>
+              </>
+            )}
+            {!loggedIn && (
+              <>
+                <MDBNavbarItem>
+                  <MDBNavbarLink href="/signup">Signup</MDBNavbarLink>
+                </MDBNavbarItem>
+                <MDBNavbarItem>
+                  <MDBNavbarLink href="/login">Login</MDBNavbarLink>
+                </MDBNavbarItem>
+              </>
+            )}
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
 }
 
 export default Navbar;
